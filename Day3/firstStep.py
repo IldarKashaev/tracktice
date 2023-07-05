384839106a0a909ff7cbed4eb1309211b9093179
YTrildA <trild1996@gmail.com>
2023-07-05 11:04:45 +0300

    fix Day3 parser

--- Day3/firstStep.py
+++ Day3/firstStep.py
@@ -5,19 +5,20 @@ from uuid import uuid4
 import urllib
 
 path = 'images/'
+links = []
 
 try:
     os.mkdir(path) #Making a new folder if it does not exist
 except: 
     print("Папка уже существует!")
 
-def pars(num):
+def carParsing(num):
     session = requests.Session()
-        
-    for x in range(num): 
+    global links
+    
+    for x in range(num+1): 
         payload = {'start': str(x*18)}
         r = session.get('https://migalki.net/images.php', params=payload) #Connecting to the Website
-        
         soup = BeautifulSoup(r.text, 'html.parser')
         
         t1, t2 = [], []
@@ -31,11 +32,19 @@ def pars(num):
             soup = BeautifulSoup(t3, 'html.parser')
         
         for link in soup.find_all('a'):
-            links = link.get('href') #Extracting links
-            image_name = f'{uuid4()}' + '.jpg' #Creating a unique name
-            urllib.request.urlretrieve(links, path + image_name) #Saving images
+            links.append(link.get('href')) #Extracting links
+        
+    print('Ссылки на изображения сформированы.')
+def imgDownloading(links, count):
+    for i in range(count):
+        image_name = f'{uuid4()}' + '.jpg' #Creating a unique name
+        urllib.request.urlretrieve(links[i], path + image_name) #Saving images
+    print('Изображения скачаны.')
 
-num = int(input('Введите количество изображений для загрузки: ')) #Finalize: downloads within the specified amount, instead of the exact value
-pars(num // 18)
+nums = int(input('Введите количество изображений для загрузки: ')) 
+carParsing(nums // 18)
+imgDownloading(links, nums)
+
+
 
 
